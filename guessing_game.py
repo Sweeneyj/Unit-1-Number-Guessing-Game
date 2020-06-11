7feaaa90179280d56b32c5fb3b6d66e3f75267e3
James Sweeney <sweeneyj744@gmail.com>
2020-06-11 14:12:49 -0700

    Update guessing_game.py
    Update with function get_guess

--- guessing_game.py
+++ guessing_game.py
@@ -1,16 +1,20 @@
 import random
 import sys
 
+def get_guess():
+  while True:
+    try:
+      print("Welcome to the number guessing game! ")
+      guess = int(input("Please pick a number between 1 and 10: "))
+      return guess
+    except ValueError:
+      print("Not a valid input, please try again.")
+      continue
+      
 def start_game():
   correct_number = random.randint(0, 10)
-  guess = 0
+  guess = get_guess()
   guess_amount = 1
-  print("Welcome to the guessing game! ")
-  try:
-    guess = int(input("Please pick a number between 1 and 10: "))
-  except ValueError:
-    print("Not a valid input. Please try again. ")
-    guess = int(input("Please pick a number between 1 and 10: "))
   while True:
     try:
       if guess < correct_number:
